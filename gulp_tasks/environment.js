8845466fd4d939e977ecd997ea9b402292d26ab1
David Rivero <davidmriverog@gmail.com>
2017-05-12 23:24:44 -0400

    avances efectuados

--- gulp_tasks/environment.js
+++ gulp_tasks/environment.js
@@ -2,13 +2,27 @@ const gulp = require('gulp');
 
 const gulpNgConfig = require('gulp-ng-config');
 const expect = require('gulp-expect-file');
+const es = require("event-stream");
+const rename = require("gulp-rename");
+const fileExists = require('file-exists');
 
 const conf = require('../conf/gulp.conf');
 
 gulp.task('environment', environments);
 
 function environments() {
-  return gulp.src(conf.path.const('environment.json'))
+
+  if(fileExists.sync(conf.path.const('environment.json'))){
+    const config = gulp.src(conf.path.const('environment.json'))
     .pipe(gulpNgConfig('app.const', {environment:'env.build', pretty:true}))
     .pipe(gulp.dest(conf.path.const()));
+
+    return config;
+  }else{
+    const create = gulp.src(conf.path.const('envexample.json'))
+    .pipe(rename('environment.json'))
+    .pipe(gulp.dest(conf.path.const()));
+
+    return es.merge(create).pipe(config);
+  }
 }

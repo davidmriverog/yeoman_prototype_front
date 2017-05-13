c3b4ffb1ec54250c0618f665a1e649be77c7a567
David Rivero <davidmriverog@gmail.com>
2017-05-13 00:12:07 -0400

    finalizamos integracion de la variable de entorno

--- gulp_tasks/environment.js
+++ gulp_tasks/environment.js
@@ -2,27 +2,34 @@ const gulp = require('gulp');
 
 const gulpNgConfig = require('gulp-ng-config');
 const expect = require('gulp-expect-file');
-const es = require("event-stream");
 const rename = require("gulp-rename");
 const fileExists = require('file-exists');
 
 const conf = require('../conf/gulp.conf');
 
 gulp.task('environment', environments);
+gulp.task('configenv',configenv);
 
-function environments() {
+function environments(done) {
 
-  if(fileExists.sync(conf.path.const('environment.json'))){
-    const config = gulp.src(conf.path.const('environment.json'))
-    .pipe(gulpNgConfig('app.const', {environment:'env.build', pretty:true}))
-    .pipe(gulp.dest(conf.path.const()));
+  if(!fileExists.sync(conf.path.const('environment.json'))){
 
-    return config;
-  }else{
     const create = gulp.src(conf.path.const('envexample.json'))
     .pipe(rename('environment.json'))
     .pipe(gulp.dest(conf.path.const()));
 
-    return es.merge(create).pipe(config);
+    return create;
+  }else{
+    console.log('environment is created!');
+    done();
   }
+}
+
+function configenv(){
+  const config = gulp.src(conf.path.const('environment.json'))
+    .pipe(gulpNgConfig('app', {environment:'env.build', pretty:true}))
+    .pipe(gulp.dest(conf.path.src('app/constants')));
+
+  console.log('config success environment!');
+  return config;
 }

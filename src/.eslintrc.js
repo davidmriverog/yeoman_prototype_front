b8ab5efaf28626504f66cebec2269bfdf9ee45bd
David M. Rivero G <davidmriverog@gmail.com>
2017-05-15 16:02:58 -0400

    fix

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,8 +1,21 @@
+
+const env = require('../conf/env.conf');
+
+const os = require('os');
+
 module.exports = {
   extends: [
     'angular'
   ],
   rules: {
-    'angular/no-service-method': 0
-  }
+    'angular/no-service-method': 0,
+    'linebreak-style': function(){
+      swith(os.type()){
+        case 'Windows_NT':
+          return ['error','windows'];
+          break;
+      }
+    }
+  },
+  
 }

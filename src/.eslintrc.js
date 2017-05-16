f63a6f32dfb66f9a6be91e4f0e8744ec28286840
David Rivero <davidmriverog@gmail.com>
2017-05-15 20:07:55 -0400

    master - ajuste de estandar de codificacion js segun el os!

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,21 +1,19 @@
-
-const env = require('../conf/env.conf');
-
 const os = require('os');
 
-module.exports = {
-  extends: [
-    'angular'
-  ],
-  rules: {
-    'angular/no-service-method': 0,
-    'linebreak-style': function(){
-      swith(os.type()){
-        case 'Windows_NT':
-          return ['error','windows'];
-          break;
-      }
-    }
-  },
+switch(os.type()){
+  case 'Windows_NT':
   
+    module.exports = {
+        extends: [
+          'angular'
+        ],
+        rules: {
+          'angular/no-service-method': 0,
+          'linebreak-style': ['error','windows']
+        }
+        
+    }
+
+    break;
 }
+
